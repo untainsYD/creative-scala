348f9c365fd0ef3c99692c3a4fec8f0940d2896e
Yarosλaβ <untainsyd@gmail.com>
2024-01-25 20:48:11 +0200

    fix: side effects example

--- src/main/scala/05_substitution_model_of_evaluation/SideEffects.scala
+++ src/main/scala/05_substitution_model_of_evaluation/SideEffects.scala
@@ -1,30 +1,33 @@
 import scala.concurrent.Future
+import concurrent.ExecutionContext.Implicits.global
 
-def computeA(): Int = { println("Open up a package of my Bologna"); 1 }
-def computeB(): Int = { println("Oh, i think the toast is done"); 2 }
+object SideEffects extends App {
+    def computeA(): Int = { println("Open up a package of my Bologna"); 1 }
+    def computeB(): Int = { println("Oh, i think the toast is done"); 2 }
 
-val fa = Future(computeA())
-val fb = Future(computeB())
+    val fa = Future(computeA())
+    val fb = Future(computeB())
 
-for {
-    a <- fa
-    b <- fb
-} yield a + b
+    for {
+        a <- fa
+        b <- fb
+    } yield a + b
 
-// Probably prints:
-// Open up a package of my Bologna
-// Oh, i think the toast is done
+    // Probably prints:
+    // Open up a package of my Bologna
+    // Oh, i think the toast is done
 
-// or prints:
-// Oh, i think the toast is done
-// Open up a package of my Bologna
+    // or prints:
+    // Oh, i think the toast is done
+    // Open up a package of my Bologna
 
-// But this code
-for {
-    a <- Future(computeA())
-    b <- Future(computeB())
-} yield a + b
+    // But this code
+    for {
+        a <- Future(computeA())
+        b <- Future(computeB())
+    } yield a + b
 
-// Will print:
-// Open up a package of my Bologna
-// Oh, i think the toast is done
+    // Will print:
+    // Open up a package of my Bologna
+    // Oh, i think the toast is done
+}
